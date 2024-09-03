03252bdc17244bda69df7cb9d7f392ccea657e63
Samuel Soares <samucosta58@gmail.com>
2024-09-03 16:04:25 -0300

    Commit inicial

---
+++ api-hendler-next14/app/api/User/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import prisma from "@/lib/db";
+
+export async function GET(req: NextRequest) {
+  console.log(req.nextUrl.searchParams);
+  return Response.json({ message: "ok" });
+}
+export async function POST(req: Request) {
+  const { name } = await req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name,
+      },
+    });
+    return Response.json({ message: "ok", user });
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        err,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
