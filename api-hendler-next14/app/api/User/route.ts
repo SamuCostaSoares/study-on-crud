03f92b360dda312d23df1623246505823b209b58
Samuel Soares <samucosta58@gmail.com>
2024-09-04 15:25:55 -0300

    update

--- api-hendler-next14/app/api/User/route.ts
+++ api-hendler-next14/app/api/User/route.ts
@@ -1,11 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
-
 import prisma from "@/lib/db";
 
 export async function GET(req: NextRequest) {
-  console.log(req.nextUrl.searchParams);
-  return Response.json({ message: "ok" });
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json({ message: "ok", users });
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        err,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
+
 export async function POST(req: Request) {
   const { name } = await req.json();
   try {
@@ -14,7 +26,55 @@ export async function POST(req: Request) {
         name,
       },
     });
-    return Response.json({ message: "okk", user });
+    return NextResponse.json({ message: "ok", user });
+  } catch (err) {
+    return NextResponse.json(
+      {
+        message: "Error",
+        err,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function DELETE(req: Request) {
+    const{ id } = await req.json();
+     try {
+        const user = await prisma.user.delete({
+          where: {
+            id
+          }
+        });
+        return Response.json({ message: "ok", user });
+     }catch (err){
+       return Response.json(
+         {
+           message: "Error",
+           err,
+         },
+         {
+           status: 500,
+         }
+       );
+     }
+}
+
+
+export async function PUT(req: Request) {
+  const { id, name } = await req.json();
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+      },
+    });
+    return NextResponse.json({ message: "ok", user });
   } catch (err) {
     return NextResponse.json(
       {
